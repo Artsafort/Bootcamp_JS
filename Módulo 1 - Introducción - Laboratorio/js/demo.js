a2084a60f61e596dd2763ac8c65ca10e0fb8af44
Artsafort <artinsafarian@live.com>
2020-08-23 15:16:56 +0200

    second exercise

---
+++ Módulo 1 - Introducción - Laboratorio/js/demo.js
@@ -0,0 +1,4 @@
+document.getElementById("name").value = "Mi nombre";
+document.getElementById("lastName").value = "Mi apellido";
+// diff
+document.getElementById("avatar").src = "Lia recuerdo.jpg";
